fc438c62d3a8cbda32c9abf3b8c64b098997bdec
Guillermo <guillermo.segura.mt@usa.edu.co>
2022-12-07 00:47:40 -0500

    Controladores

---
+++ MiAplicacionClase3/controllers/userController.js
@@ -0,0 +1,67 @@
+const users = []
+
+//Recibe un Objeto Json
+
+export function createUser(req, res){
+
+    //const nombre = userData.nombre
+    //const apellido = userData.apellido
+    //const email = userData.email
+    const userData = req.body.userData;
+
+    if (userData == null) {
+        res.status(400).json("Es necesario atribuir userData")
+        return;
+    }
+
+    const {nombre, apellido, email} = userData
+
+    if(nombre == null){
+        res.status(400).json("Necesita un nombre")
+        return;
+    }
+
+    if(apellido == null){
+        res.status(400).json("Necesita un apellido")
+        return;
+    }
+
+    if(email == null){
+        res.status(400).json("Necesita un email")
+        return;
+    }
+
+    const user = {
+        nombre,
+        apellido,
+        email
+    }
+
+    users.push(user)
+
+    res.status(200).json(user)
+}
+
+//READ
+export function readUser(req, res){
+    
+    const nombre = req.params.nombre
+
+
+    for (let index = 0; index < users.length; index++) {
+
+        
+        const user = users[index]
+
+        console.log(user);
+
+        if (nombre == user.nombre){
+            res.status(200).json(user)
+        return;
+        }
+    }
+
+    res.sendStatus(200)
+    
+}
+
--- MiAplicacionClase3/routes/users.js
+++ MiAplicacionClase3/routes/users.js
@@ -1,8 +1,7 @@
 import express from "express"
+import { createUser, readUser } from "../controllers/userController.js"
 
 const router = express.Router()
-
-
 
 // ./users/new
 const usuarios = []
@@ -12,29 +11,13 @@ router.get("/", function (req, res){
 })
 
 router.post("/new", (req, res) => {
-
-    const nombre = req.body.nombre;
-    const correo = req.body.correo;
-    const edad = req.body.edad;
-    const id = Math.random().toString(36).slice(2)
-
-    const usuario = {
-        nombre: nombre,
-        correo: correo,
-        edad: edad,
-        id:id
-    }
-
-    usuarios.push(usuario)
-    res.json(usuario)
-    
+    createUser(req, res)
 })
 
-router.get("/:args", (req, res) => {
+router.get("/:nombre", (req, res) => {
 
-    let args = req.params.args;
-    args = args.split(";")
-    res.json(args[4])
+    readUser(req, res)
+  
 })
 
 
